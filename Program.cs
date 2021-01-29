df8c5fe985745e01ebcdb4148c9fb051370c76dc
Tim <nigh.timothy@gmail.com>
2021-01-29 12:28:04 -0500

    added conversions from hex to decimal Added option to make sure things are converting correctly

--- Program.cs
+++ Program.cs
@@ -11,10 +11,46 @@ namespace hexConverter
             string input = "";
             while (input != "Q")
             {
-                Console.WriteLine("Please enter a decimal number to convert to hex, Q to exit");
+                Console.WriteLine("enter 1 to convert dec to hex, 2 to convert hex to dec, Q to exit");
                 input = Console.ReadLine();
-                if(int.TryParse(input,out int nInput))
-                 Console.WriteLine(converter.decimalToHex(nInput));
+                if (int.TryParse(input, out int result))
+                {
+                    if (result == 1)
+                    {
+                        while (input != "Q")
+                        {
+                            Console.WriteLine("Please enter a decimal number to convert to hex, Q to exit");
+                            input = Console.ReadLine();
+                            if (int.TryParse(input, out int nInput))
+                                Console.WriteLine(converter.decimalToHex(nInput));
+                        }
+                    }
+                    else if (result == 2)
+                    {
+                        while (input != "Q")
+                        {
+                            Console.WriteLine("Please enter a hex number to convert to hex, Q to exit");
+                            input = Console.ReadLine();
+                            if (input != "Q")
+                                Console.WriteLine(converter.hexToDec(input));
+                        }
+                    }
+                    else if (result == 3)
+                    {
+                        while (input != "Q")
+                        {
+                            Console.WriteLine("Please enter a number");
+                            input = Console.ReadLine();
+                            if (int.TryParse(input, out int nInput)) 
+                            {
+                                string hex = converter.decimalToHex(nInput);
+                                Console.WriteLine($"Your number in hex: {hex}");
+                                Console.WriteLine($"Your number converted back: {converter.hexToDec(hex)}");
+                            }
+
+                        }
+                    }
+                }
             }
             Console.WriteLine("Exiting, press any key to close window");
             Console.ReadKey();
--- converter.cs
+++ converter.cs
@@ -39,10 +39,60 @@ namespace hexConverter
                 }
                 input = input / 16;
             } while (input != 0) ;
-            return StackToString(outputStack);
+            return stackToString(outputStack);
         }
 
-        private string StackToString(Stack<string> input)
+        public int hexToDec(string input)
+        {
+            int maxPower = input.Length;
+            int power = 0;
+            int output = 0;
+            Stack<char> hexDigits = new Stack<char>();
+            foreach(char c in input)
+            {
+                hexDigits.Push(c);
+            }
+            while(hexDigits.Count != 0)
+            {
+                char digit = hexDigits.Pop();
+                int decDigit = 0;
+                ;
+                switch(digit)
+                {
+                    case 'A':
+                        decDigit = 10;
+                        break;
+                    case 'B':
+                        decDigit = 11;
+                        break;
+                    case 'C':
+                        decDigit = 12;
+                        break;
+                    case 'D':
+                        decDigit = 13;
+                        break;
+                    case 'E':
+                        decDigit = 14;
+                        break;
+                    case 'F':
+                        decDigit = 15;
+                        break;
+                    default:
+                        decDigit = (int)Char.GetNumericValue(digit);
+                        break;
+                }
+                output += decDigit * (int)Math.Pow(16,power);
+                if(power<maxPower)
+                {
+                    power++;
+                }
+            }
+
+            return output;
+            
+        }
+
+        private string stackToString(Stack<string> input)
         {
             StringBuilder builder = new StringBuilder();
             while(input.Count !=0)
